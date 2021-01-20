d7333c1da65240814e7e3d64ef94a155dcafc667
Jozsef Ottucsak <ottucsakjozsef@gmail.com>
2021-01-20 18:01:55 +0100

    gitignoreC:/Python/Python37/python.exe

--- testAquarium.py
+++ testAquarium.py
@@ -1,15 +1,21 @@
 import unittest
 from Aquarium import Aquarium
+from Fish import Tang
 
 class AquariumTest(unittest.TestCase):
 
-    testAq = Aquarium()
+    def testInit(self):
+        aq = Aquarium([Tang("test", 1, 'red', False)])
+        self.assertEqual(len(aq.fishes), 1)
 
     def testAdd():
-
+        pass
 
     def testfeed():
+        pass
 
     def testremoveFish():
+        pass
 
     def testgetstatus():
+        pass
