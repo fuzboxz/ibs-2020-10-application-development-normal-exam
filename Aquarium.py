6d073b019abdfef20c8a5db68364e9d994b47fb7
Jozsef Ottucsak <ottucsakjozsef@gmail.com>
2021-01-20 18:32:26 +0100

    aquarium and tests

--- Aquarium.py
+++ Aquarium.py
@@ -1,24 +1,37 @@
+from typing import List
+from Fish import Fish
+from os import linesep
+
 class Aquarium:
 
     def __init__(self, fishes=None):
-        if fishes is not None:
+        if isinstance(fishes, list):
             self.fishes = fishes
-        else:
+        elif isinstance(fishes, type(None)):
             self.fishes = []
+        elif isinstance(fishes, Fish) or isinstance(fishes.super(), Fish):
+            self.fishes = [fishes]
+            
 
-    def addFish(self, fish):
-        self.fishes.append(fish)
-        return True
+
+    def addFish(self, fishes):
+        if isinstance(fishes, List):
+            self.fishes.extend(fishes)
+        elif isinstance(fishes, Fish) or isinstance(fishes.super(), Fish):
+            self.fishes.append(fishes)
 
     def feed(self):
         for fish in self.fishes:
             fish.feed()
 
     def removeFish(self):
-        for i in len(self.fishes):
-            if self.fishes[i].weight <= 11:
+        for i in range(len(self.fishes)):
+            if self.fishes[i].weight >= 11:
                 self.fishes.remove(self.fishes[i])
 
     def getStatus(self):
+        text = ""
         for fish in self.fishes:
+            text += fish.status()
             print(fish.status())
+        return text
--- testAquarium.py
+++ testAquarium.py
@@ -1,21 +1,89 @@
 import unittest
 from Aquarium import Aquarium
-from Fish import Tang
+from Fish import Tang, Clownfish
+
 
 class AquariumTest(unittest.TestCase):
 
     def testInit(self):
+        # single element in list
         aq = Aquarium([Tang("test", 1, 'red', False)])
         self.assertEqual(len(aq.fishes), 1)
+        self.assertIsInstance(aq.fishes[0], Tang)
 
-    def testAdd():
-        pass
+        # multiple elements in list
+        aq = Aquarium([Tang("test", 1, 'red', False), Clownfish("test", 1, 'red', 'blue')])
+        self.assertEqual(len(aq.fishes), 2)
+        self.assertIsInstance(aq.fishes[1], Clownfish)
 
-    def testfeed():
-        pass
+        # empty list
+        aq = Aquarium()
+        self.assertEqual(aq.fishes, [])
 
-    def testremoveFish():
-        pass
+    def testAdd(self):
+        # add single fish to tank
+        aq = Aquarium()
+        self.assertEqual(aq.fishes, [])
+        aq.addFish(Tang("test", 1, 'red', False))
+        self.assertEqual(len(aq.fishes), 1)
+        self.assertIsInstance(aq.fishes[0], Tang)
 
-    def testgetstatus():
-        pass
+        # add list of fishes to tank
+        aq = Aquarium()
+        self.assertEqual(aq.fishes, [])
+        aq.addFish([Tang("test", 1, 'red', False)])
+        self.assertEqual(len(aq.fishes), 1)
+        self.assertIsInstance(aq.fishes[0], Tang)
+
+        # add list with multiple elements to tank
+        aq = Aquarium()
+        self.assertEqual(aq.fishes, [])
+        aq.addFish([Tang("test", 1, 'red', False), Clownfish("test", 1, 'red', 'blue')])
+        self.assertEqual(len(aq.fishes), 2)
+        self.assertIsInstance(aq.fishes[1], Clownfish)
+        
+        # add multiple elements to a list with existing elements
+        aq = Aquarium(Tang("test", 1, 'red', False))
+        aq.addFish([Tang("test", 1, 'red', False), Clownfish("test", 1, 'red', 'blue')])
+        self.assertEqual(len(aq.fishes), 3)
+
+    def testfeed(self):
+        aq = Aquarium([Tang("test", 1, 'red', False)])
+        aq.feed()
+        self.assertEqual(len(aq.fishes), 1)
+        self.assertIsInstance(aq.fishes[0], Tang)
+        self.assertEqual(aq.fishes[0].weight, 2)
+
+    def testremoveFish(self):
+        # under 11
+        aq = Aquarium([Tang("test", 10, 'red', False)])
+        self.assertEqual(len(aq.fishes), 1)
+        aq.removeFish()
+        self.assertEqual(len(aq.fishes), 1)
+
+        # 11
+        aq = Aquarium([Tang("test", 11, 'red', False)])
+        aq.removeFish()
+        self.assertEqual(len(aq.fishes), 0)
+
+        # above 11
+        aq = Aquarium([Tang("test", 12, 'red', False)])
+        self.assertEqual(len(aq.fishes), 1)
+        aq.removeFish()
+        self.assertEqual(len(aq.fishes), 0)
+
+    def testgetstatus(self):
+        # single element
+        aq = Aquarium([Tang("test", 12, 'red', False)])
+        self.assertEqual(aq.getStatus(), "test, weight: 12, color: red, short-term memory loss: false")
+
+        # empty list
+        aq = Aquarium()
+        self.assertEqual(aq.getStatus(), '')
+
+        # two lists
+        aq = Aquarium([Tang("test", 1, 'red', False), Clownfish("test", 1, 'red', 'blue')])
+        self.assertEqual(aq.getStatus(), "test, weight: 1, color: red, short-term memory loss: falsetest, weight: 1, color: red, stripes: blue")
+
+if __name__ == "__main__":
+    unittest.main()
